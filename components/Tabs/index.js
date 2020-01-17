fc0ee9fb5b739c4d81c905e79a43713fd010e0a4
Juana Villalpando <juana.v14@gmail.com>
2020-01-17 09:19:02 -0800

    complete tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then (response =>{
+        console.log(response)
+        const tabsDiv = Tab(response.data.topics);
+    })
+    .catch(error =>{
+        console.log('The data is not responding', error);
+    });
+
+    function Tab(topics){
+        const entryPoint = document.querySelector('.topics');
+        topics.forEach((topic) => {
+            const tabTop = document.createElement('div');
+            tabTop.classList.add('tab');
+            tabTop.textContent = topic;
+            entryPoint.appendChild(tabTop);
+        });
+    }
+    
